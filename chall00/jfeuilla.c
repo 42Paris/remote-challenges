7507a92a362a976af6f1cedee668d78d07e86878
loaki <55529264+loaki@users.noreply.github.com>
2020-04-06 14:47:31 +0200

    Create jfeuilla.c

---
+++ chall00/jfeuilla.c
@@ -0,0 +1,31 @@
+int ft_necklace(char *s1, char *s2)
+{
+	int i;
+	int	j;
+	int len;
+	int save;
+
+	i = 0;
+	while (s1[i])
+	{
+		j = 0;
+		if (s1[i] == s2[j])
+		{
+			save = i;
+			while (s2[j] && s1[i] && s1[i] == s2[j])
+			{
+				i++;
+				j++;
+				if (s1[i] == 0)
+					i = 0;
+				if (s2[j] != 0)
+					len++;
+			}
+			if (len == j)
+				return (1);
+			i = save;
+		}
+		i++;
+	}
+	return (0);
+}
