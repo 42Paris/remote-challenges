2d5ec7ab364e328bedacf8a5d21cd7e17996d33a
Ferdinand Ratajczak <fe.ratajczak@gmail.com>
2020-04-06 14:18:59 +0200

    done ?

--- chall00/fratajcz.c
+++ chall00/fratajcz.c
@@ -5,16 +5,20 @@ int		ft_necklace(char *s1, char *s2)
 	int j;
 	if (!*s1 && !*s2)
 		return (1);
+	if ((*s1 && !*s2) || (!*s1 && *s2))
+		return (0);
 	while (s1[i])
 	{
 		j = i;
 		k = 0;
-		while (s1[k] == s2[j] && s2[j])
+		while (s1[j] == s2[k] && s2[k])
 		{
 			k++;
 			j++;
+			if (s1[j] == 0)
+				j = 0;
 		}
-		if (s1[j] == 0)
+		if (s2[k] == 0)
 			return (1);
 		i++;
 	}
