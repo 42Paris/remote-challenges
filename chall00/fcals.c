ea189f2d5720bbb763dcd803818fa47daa07d700
Midarius <26384743+Midarius@users.noreply.github.com>
2020-04-06 18:10:08 +0200

    string literal proof + fixed empty string case

--- chall00/fcals.c
+++ chall00/fcals.c
@@ -6,9 +6,28 @@
 /*   By: fcals <fcals@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/06 15:15:03 by fcals             #+#    #+#             */
-/*   Updated: 2020/04/06 16:50:05 by fcals            ###   ########.fr       */
+/*   Updated: 2020/04/06 18:07:42 by fcals            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
+
+static char			*ft_strndup(const char *s, unsigned int size)
+{
+	char			*ptr;
+	unsigned int	i;
+
+	if (!(ptr = malloc(sizeof(char) * size + 1)))
+		return (NULL);
+	ptr[size] = '\0';
+	i = 0;
+	while (i < size)
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	return (ptr);
+}
 
 static unsigned int	ft_strlen(const char *s)
 {
@@ -57,18 +76,27 @@ int					ft_necklace(char *s1, char *s2)
 {
 	unsigned int	size;
 	unsigned int	i;
+	char			*ptr;
 
 	if (!s1 || !s2)
 		return (-1);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 	if ((size = ft_strlen(s1)) != ft_strlen(s2))
 		return (0);
+	if (!(ptr = ft_strndup(s1, size)))
+		return (-1);
 	i = 0;
 	while (i < size)
 	{
-		strshift(s1, size);
-		if (ft_strequ(s1, s2))
+		strshift(ptr, size);
+		if (ft_strequ(ptr, s2))
+		{
+			free(ptr);
 			return (1);
+		}
 		i++;
 	}
+	free(ptr);
 	return (0);
 }
