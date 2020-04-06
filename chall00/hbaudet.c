9f64bab8c11e82bb2f6d796075f1027992525d31
hbaudet <hbaudet@student.42.fr>
2020-04-06 17:04:17 +0200

    hbaudet's --awesome-- (or not) solution to this epic problem, now with extra-protection

--- chall00/hbaudet.c
+++ chall00/hbaudet.c
@@ -6,11 +6,10 @@
 /*   By: hbaudet <hbaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/06 16:26:03 by hbaudet           #+#    #+#             */
-/*   Updated: 2020/04/06 16:32:07 by hbaudet          ###   ########.fr       */
+/*   Updated: 2020/04/06 17:03:18 by hbaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <string.h>
 
 int	ft_necklace(char *s1, char *s2)
@@ -20,9 +19,10 @@ int	ft_necklace(char *s1, char *s2)
 	size_t	j;
 
 	i = 0;
-	len = strlen(s1);
-	j = strlen(s2);
-	if (len != j)
+	if (!s1 && !s2)
+		return (1);
+	if (!s1 || !s2 || ((len = strlen(s1))
+		!= (j = strlen(s2))))
 		return (0);
 	if (!len)
 		return (1);
@@ -30,9 +30,7 @@ int	ft_necklace(char *s1, char *s2)
 	{
 		j = 0;
 		while (s1[(i + j) % len] == s2[j])
-		{
 			j++;
-		}
 		if (j == len)
 			return (1);
 		i++;
