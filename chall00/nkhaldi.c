41c28d4b87350db3fc75b68e951f9371605f86b5
Nay <nkhaldi@student.42.fr>
2020-04-06 14:31:42 +0200

    solution

---
+++ chall00/nkhaldi.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_nakelace(char *s1, char *s2)
+{
+	int ret = 0;
+	int i = 0;
+	int j = 0;
+
+	if (strlen(s1) != strlen(s2))
+		return ret;
+	if (!strcmp(s1, "") && !strcmp(s2, ""))
+		return (ret = 1);
+	while (i < strlen(s1))
+	{
+		if (s1 && (s1[i] == s2[j]))
+		{
+			j++;
+			ret = 1;
+		}
+		else
+		{
+			ret = 0;
+		}
+		i++;
+	}
+
+	return (ret);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("result : [%d]\n", ft_nakelace(argv[1], argv[2]));
+	}
+	return 0;
+}
