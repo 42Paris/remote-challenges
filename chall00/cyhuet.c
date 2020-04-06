f1fbec15d508b365201d458693d93bb612c1bd44
Cyprien Huet <cyprienhuet@gmail.com>
2020-04-06 17:45:24 +0200

    no leaks

--- chall00/cyhuet.c
+++ chall00/cyhuet.c
@@ -20,15 +20,28 @@ static char	*shift_char(char *s1)
 
 int ft_necklace(char *s1, char *s2)
 {
+	s1 = strdup(s1);
 	if (strlen(s1) != strlen(s2))
 		return 0;
 	if (strlen(s1) == 0 && strlen(s2) == 0)
 		return (1);
 	for (int i = 0; i < strlen(s1); i++)
 	{
+		char *tmp = s1;
 		s1 = shift_char(s1);
+		free(tmp);
 		if (strcmp(s1, s2) == 0)
+		{
+			free(s1);
 			return (1);
+		}
 	}
+	free(s1);
 	return (0);
+}
+
+int main()
+{
+	ft_necklace("nicole", "icolen");
+	return 0;
 }
