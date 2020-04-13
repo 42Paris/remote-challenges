e382b3f7529c2484c86921b5f11d19511d350104
admin <admin@iMac-de-admin-8.local>
2020-04-13 15:36:49 +0200

    etieberg

---
+++ chall01/etieberg.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *ft_rgb2hex(int r, int g, int b)
+{
+	unsigned long	rgb;
+	char		*str;
+
+	if ((r < 0 || r > 255) || (g < 0 || g > 255) || (b < 0 || b > 255))
+		return (NULL);
+	if (!(str = (char*)malloc(sizeof(char) * 8)))
+		return (NULL);
+	str[0] = '#';
+	rgb = ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+	str[7] = '\0';
+	sprintf(str+1,"%06lx", rgb);
+	return (str);
+}
