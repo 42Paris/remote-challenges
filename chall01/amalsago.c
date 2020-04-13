49b30b71c5600ee65cb517238c6398c09a45dcd2
4slan <amalsago@student.42.fr>
2020-04-13 18:12:59 +0200

    hotfix: add verification for negative numbers

--- chall01/amalsago.c
+++ chall01/amalsago.c
@@ -6,7 +6,7 @@
 /*   By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 11:46:30 by amalsago          #+#    #+#             */
-/*   Updated: 2020/04/13 12:37:25 by amalsago         ###   ########.fr       */
+/*   Updated: 2020/04/13 18:11:22 by amalsago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	hex[8];
 
 char	*ft_rgb2hex(int r, int g, int b)
 {
+	if (r < 0 || g < 0 || b < 0)
+		return (NULL);
 	hex[0] = '#';
 	sprintf(hex + 1, "%02x", r);
 	sprintf(hex + 3, "%02x", g);
