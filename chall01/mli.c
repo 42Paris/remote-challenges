818cb7d0d22afe52e717ba4ea94d121782155461
mli <mli@student.42.fr>
2020-04-13 18:04:48 +0200

    mli (better)

--- chall01/mli.c
+++ chall01/mli.c
@@ -6,22 +6,36 @@
 /*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 17:32:56 by mli               #+#    #+#             */
-/*   Updated: 2020/04/13 17:47:49 by mli              ###   ########.fr       */
+/*   Updated: 2020/04/13 18:04:27 by mli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
+#include <stdlib.h>
 
 /* Explication : C'est un triple itoa fait a la main */
 
+const char *base = "0123456789abcdef";
+
 char *ft_rgb2hex(int r, int g, int b)
 {
-	char *base = "0123456789abcdef";
-	char ret[8];
+	char *ret;
 
-	ret[0] = '#'; ret[7] = 0;
+	if (!(ret = malloc(sizeof(char) * 8)))
+		return (NULL);
+	ret[0] = '#'; ret[7] = '\0';
 	ret[2] = base[r % 16]; ret[1] = base[(r / 16) % 16];
 	ret[4] = base[g % 16]; ret[3] = base[(g / 16) % 16];
 	ret[6] = base[b % 16]; ret[5] = base[(b / 16) % 16];
-	return (strdup(ret));
+	return (ret);
 }
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int		main(void)
+{
+	printf("%s\n", ft_rgb2hex(252, 186, 3));
+	printf("%d\n", strcmp(ft_rgb2hex(252, 186, 3), "#fcba03"));
+}
+*/
