97ea1cf1d737e85e0fab6a689faeed10543c95cf
mbourand <mbourand@student.42.fr>
2020-04-13 14:32:25 +0200

    ft_rgb2hex

---
+++ chall01/mbourand.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *ft_rgb2hex(int r, int g, int b)
+{
+	char *base = "0123456789abcdef";
+	char *hex = malloc(sizeof(char) * 8);
+
+	hex[0] = '#';
+	hex[1] = base[r / 16];
+	hex[2] = base[r % 16];
+	hex[3] = base[g / 16];
+	hex[4] = base[g % 16];
+	hex[5] = base[b / 16];
+	hex[6] = base[b % 16];
+	hex[7] = 0;
+
+	return hex;
+}
