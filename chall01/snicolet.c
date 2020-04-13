853998383465da7e87f4c0a60b58dd06b6797401
Sébastien NICOLET <snicolet@student.42.fr>
2020-04-13 14:54:45 +0200

    just a little security on color filtering

--- chall01/snicolet.c
+++ chall01/snicolet.c
@@ -12,13 +12,16 @@
 ** We create a color from r, g and b (considered as char)
 ** using binary moves to place the correct bits at the right place,
 ** then we pass that color to printf, and return the allocated string.
+** also a filter of 0-255 validity is added on each color componment
 */
 
 char        *ft_rgb2hex(int r, int g, int b)
 {
     char            *color_str;
+    unsigned int    color;
 
-    if (asprintf(&color_str, "#%08x", (r << 16) | (g << 8) | b) < 0)
+    color = ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+    if (asprintf(&color_str, "#%08x", color) < 0)
         return (NULL);
     return (color_str);
 }
