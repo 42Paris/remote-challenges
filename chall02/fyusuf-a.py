d8be30b467a1e7c2bb52e6eee9d5bb556fcfa942
fyusuf-a <fyusuf-a@student.42.fr>
2020-04-20 14:54:28 +0200

    fyusuf-a

---
+++ chall02/fyusuf-a.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) != 2:
+    print("usage: ./fyusuf-a <a-zA-Z string>")
+    sys.exit(0)
+if sys.argv[1] == "":
+    print("usage: ./fyusuf-a <a-zA-Z string>")
+    sys.exit(0)
+str = sys.argv[1]
+for i in range(len(str)):
+    if not str[i].isalpha() and not str[i] == ' ':
+        print("usage: ./fyusuf-a <a-zA-Z string>")
+        sys.exit(0)
+# if not sys.argv[1].isalpha():
+    # print("usage: ./fyusuf-a <a-zA-Z string>")
+    # sys.exit(0)
+str = sys.argv[1].lower()
+alphabet = {
+   'a':'.-',
+   'b':'-...',
+   'c':'-.-.',
+   'd':'-..',
+   'e':'.',
+   'f':'..-.',
+   'g':'--.',
+   'h':'....',
+   'i':'..',
+   'j':'.---',
+   'k':'-.-',
+   'l':'.-..',
+   'm':'--',
+   'n':'-.',
+   'o':'---',
+   'p':'.--.',
+   'q':'--.-',
+   'r':'.-.',
+   's':'...',
+   't':'-',
+   'u':'..-',
+   'v':'...-',
+   'w':'.--',
+   'x':'-..-',
+   'y':'-.--',
+   'z':'--..',
+   ' ':''
+}
+ret = ''
+for i in range(len(str)):
+    ret += alphabet[str[i]]
+print(ret, sep='')
