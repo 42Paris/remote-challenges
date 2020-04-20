bbaf3b1dd416f36cf10d3079ff99898bed6377f9
cdittric <cdittric@student.42.fr>
2020-04-20 14:41:57 +0200

    [Chall02] Add cdittric contribution

---
+++ chall02/cdittric.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import sys
+
+morse = [
+".-",
+"-...",
+"-.-.",
+"-..",
+".",
+"..-.",
+"--.",
+"....",
+"..",
+".---",
+"-.-",
+".-..",
+"--",
+"-.",
+"---",
+".--.",
+"--.-",
+".-.",
+"...",
+"-",
+"..-",
+"...-",
+".--",
+"-..-",
+"-.--",
+"--..",
+]
+
+if (len(sys.argv) != 2):
+    print("usage: ", sys.argv[0], " <a-zA-Z string>")
+    sys.exit();
+
+for c in sys.argv[1]:
+    if not (c.isalpha() or c.isspace()):
+        print("usage: ", sys.argv[0], " <a-zA-Z string>")
+        sys.exit();
+
+for c in sys.argv[1]:
+    if c == ' ':
+        print(' ', end='')
+    elif c.islower():
+        print(morse[ord(c) - ord('a')], end='')
+    else:
+        print(morse[ord(c) - ord('A')], end='')
+print("")
