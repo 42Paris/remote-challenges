e07030a709282fc69f4c039c3afd2c26bf132af2
fyusuf-a <fyusuf-a@student.42.fr>
2020-05-04 18:16:38 +0200

    fyusuf-a's solution to chall04

---
+++ chall04/fyusuf-a.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import sys
+
+def atoi(start, string):
+    ret = 0
+    while start < len(string) and string[start].isdigit():
+        ret = ret * 10 + int(string[start])
+        start += 1
+    return (ret, start)
+
+def parse_newline(start, string):
+    if string[start] != "\n":
+        print("Expected a newline")
+        sys.exit(1)
+    return (start + 1)
+
+def parse_token(start, string):
+    if string[start] not in [" ", "#", "."]:
+        print("Wrong token")
+        sys.exit(1)
+    return (string[start], start + 1)
+
+def print_tab(tab, dim):
+    for i in range(dim):
+        for j in range(dim):
+            print(tab[i][j], sep="", end="")
+        print("")
+
+def loop(tab, dim):
+    updated = 0
+    for i in range(dim):
+        for j in range(dim):
+            if tab[dim - i - 1][j] == ' ' and 0 <= dim - i - 2 and tab[dim - i - 2][j] == '.':
+                tab[dim - i - 1][j] = '.'
+                tab[dim - i - 2][j] = ' '
+                updated = 1
+    if updated == 0:
+        print_tab(tab, dim)
+        exit(0)
+    loop(tab, dim)
+
+parse_str = sys.stdin.read()
+(dim, index) = atoi(0, parse_str)
+if dim == 0:
+    sys.exit(0)
+index = parse_newline(index, parse_str)
+remaining_chars = len(parse_str[index:])
+if remaining_chars != (dim + 1) * dim:
+    print("dim is", dim, ": map description should not have", remaining_chars, "characters")
+    sys.exit(1)
+
+tab = []
+for i in range(dim):
+    tab.append([])
+    for j in range(dim):
+        (ret, index) = parse_token(index, parse_str)
+        tab[i].append(ret)
+    index = parse_newline(index, parse_str)
+
+loop(tab, dim)
