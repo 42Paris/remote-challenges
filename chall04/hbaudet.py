4bd1ac0d96c5fa542ff40dae968fe9bf517b111d
hbaudet <hbaudet@student.42.fr>
2020-05-04 17:08:09 +0200

    hbaudet's solution

---
+++ chall04/hbaudet.py
@@ -0,0 +1,39 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    hbaudet.py                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: hbaudet <hbaudet@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/05/04 16:04:40 by hbaudet           #+#    #+#              #
+#    Updated: 2020/05/04 17:05:07 by hbaudet          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+
+line = input().lstrip()
+if not line.isdigit():
+	exit("Invalid size : " + line)
+size = int(line)
+ret = []
+for i in range (size):
+	line = sys.stdin.readline()
+	line = line.replace('\n', '')
+	if len(line) != size:
+		exit("Invalid line : " + line)
+	for c in line:
+		if "#. ".find(c) == -1: 
+			exit("Invalid character : " + c)
+	ret.append(list(line))
+
+for i in range (size - 1):
+	for j in range (size):
+		if ret[i][j] == '.' and ret[i + 1][j] == ' ':
+			ret[i][j] = ' '
+			ret[i + 1][j] = '.'
+
+for i in range (size):
+	for j in range (size):
+		print(ret[i][j], end='')
+	print()
